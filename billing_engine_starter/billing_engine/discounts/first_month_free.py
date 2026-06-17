9f0c2cd704a9cdbafce5cec8e292bd8d77c7a631
Swetapadma Parida <ece.25becg33@silicon.ac.in>
2026-06-17 12:53:46 +0530

    Remove TODO comment in FirstMonthFree discount

--- billing_engine_starter/billing_engine/discounts/first_month_free.py
+++ billing_engine_starter/billing_engine/discounts/first_month_free.py
@@ -12,7 +12,7 @@ from billing_engine.discounts.base import Discount, DiscountContext
 
 class FirstMonthFree(Discount):
     def apply(self, subtotal: Money, context: DiscountContext) -> Money:
-        # TODO Day 1
+        
         DiscountContext) -> Money:
         if context.invoice_count_so_far == 0:
             return subtotal
